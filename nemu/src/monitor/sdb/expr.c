0f753bb7dbbe83334573a71f0a9e38483f02f279
tracer-ics2023 <tracer@njuics.org>
2023-10-13 22:54:02 +0800

    >  compile NEMU 221240073 李恒济 Linux hengji-virtual-machine 6.2.0-34-generic #34~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Sep  7 13:12:03 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:54:02 up  7:53,  1 user,  load average: 0.22, 0.27, 0.33

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -423,7 +423,7 @@ uint32_t eval(int p,int q,bool *ok){
 		uint32_t val1=eval(p,op-1,&isok1);
 		
 		uint32_t val2=eval(op+1,q,&isok2);
-		assert(0);
+//		assert(0);
 		if(!isok2){
 			*ok=false;
 			return 0;
