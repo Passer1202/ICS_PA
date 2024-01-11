f9ce2710e8a81d854f71a80d2223423bd1624148
tracer-ics2023 <tracer@njuics.org>
2024-01-11 15:43:25 +0800

    >  compile NEMU 221240073 李恒济 Linux hengji-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:43:25 up 41 min,  1 user,  load average: 0.24, 0.39, 0.39

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,7 +5,10 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  size_t cnt=0;
+  while(*s++!='\0')
+   cnt++;
+  return cnt;
 }
 
 char *strcpy(char *dst, const char *src) {
