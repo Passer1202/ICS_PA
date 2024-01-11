653ae30e4c5b885b1b7908d2fd5ba61cd9b8d654
tracer-ics2023 <tracer@njuics.org>
2024-01-11 14:27:33 +0800

    >  compile NEMU 221240073 李恒济 Linux hengji-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:27:33 up  5:10,  1 user,  load average: 0.15, 0.41, 0.33

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -7,7 +7,7 @@
 
 int printf(const char *fmt, ...) {
   //panic("Not implemented");
-  char out[100000];//buffer,可能不够大？
+  char out[8192]={'\0'};;//buffer,可能不够大？
   va_list ap;
   va_start(ap,fmt);
   int ret=vsprintf(out,fmt,ap);
