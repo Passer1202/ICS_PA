ee5fe744fa4527be77efa1daf2deafae2e502ee9
tracer-ics2023 <tracer@njuics.org>
2024-01-11 15:37:39 +0800

    >  compile NEMU 221240073 李恒济 Linux hengji-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:37:39 up 35 min,  1 user,  load average: 0.86, 0.53, 0.42

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -6,79 +6,66 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  //panic("Not implemented");
-  char* start=out;
-  
-  //va_start(ap,fmt);
-  
-  while(*fmt!='\0'){
-    if(*fmt=='%'){
-      switch(*++fmt){
-        case'd':{
-          int32_t num=va_arg(ap,int);
-           //panic("Not implemented");
-         if(num<0){
-          *out++='-';
-          num=-num;
-          }
-         if(num==0){*out++='0';}
-          else{
-		  int cnt=0;
-		  char src[100];
-		  while(num!=0){
-            src[cnt++]=(num%10+'0');
-		    num/=10;
-		  }
-		  cnt--;
-		  while(cnt>=0){
-		    *out++=src[cnt--];
-		    *out++='\0';
-		  }
-          }
-          break;
-          }
-        
-        case 's':{
-          char* str=va_arg(ap,char*);
-          while(*str!='\0'){
-            *out++=*str++;
-          }
-          *out='\0';
-          break;
-          }
-      }
-      fmt++;
-    }
-    else{
-      *out++=*fmt++;//maybe wrong
-    }
-    
+static void reverse(char *s, int len) {
+  char *end = s + len - 1;
+  char tmp;
+  while (s < end) {
+    tmp = *s;
+    *s = *end;
+    *end = tmp;
+    s++;
+    end--;
   }
-  
-  *out='\0';
-  
-  return (out-start-1);
+}
 
+static void itoa(int n, char *s) {
+  int i = strlen(s), sign = n, bit;
+  if (sign < 0) {n = -n;s[i++]='-';s[i]='\0';}
+    char st[100];
+    char*p=st;
+    char*q=st;
+  do {
+    bit=n%10;
+    *p='0'+bit;
+    p++;
+    n/=10;
+  } while (n > 0);
+  *p = '\0';
+  reverse(q, p-q);
+  strcat(s,q);
 }
 
 int printf(const char *fmt, ...) {
-  //panic("Not implemented");
-  char out[8192]={'\0'};;//buffer,可能不够大？
-  va_list ap;
-  //int ret=0;
-  va_start(ap,fmt);
-  
-  vsprintf(out,fmt,ap);
-  //if(ret>=100000||ret<0)ret=-1;
-  va_end(ap);
-  //int index_printf=0;
-  //while(out[index_printf]!='\0'){
-  	//putch(' ');
-  	//putch(out[index_printf++]);
-  	//putch(' ');
-  //}
-  return 0;
+  char buffer[8192]={'\0'};
+  va_list aptr;
+  int number=0;
+  va_start(aptr,fmt);
+  number=vsprintf(buffer,fmt,aptr);
+  va_end(aptr);
+  for(int i=0;buffer[i]!='\0';i++){
+    putch(buffer[i]);
+  }
+  return number;
+}
+
+int vsprintf(char *out, const char *fmt, va_list ap) {
+  char *start = out;
+  for (; *fmt != '\0'; fmt++) 
+  {
+    if (*fmt != '%') {
+      out=start+strlen(start);
+      *out = *fmt;
+    } 
+    else {
+      switch (*(++fmt)) {
+      case 'd': itoa(va_arg(ap, int), start); break;
+      case 'c': char c = va_arg(ap, int);*(start+strlen(start))=c; *(start+strlen(start))='\0';break;
+      case 's': char *s = va_arg(ap, char*); strcat(start, s); break;
+      }
+    }
+   }
+    *(start+strlen(start))='\0';
+    return strlen(start);
 }
 
 
