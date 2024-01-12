1ac7bc9452ec1c0594bcd5abb62365af74e9aacf
tracer-ics2023 <tracer@njuics.org>
2024-01-12 12:27:59 +0800

    >  compile NEMU 221240073 李恒济 Linux hengji-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:27:59 up  4:13,  1 user,  load average: 0.33, 0.40, 0.47

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -41,7 +41,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   //uint32_t hig = inl(VGACTL_ADDR)&vga_mask;
   
   //printf("wid=%d\n",wid);
-  for (int i = y; (i < 300); i++) {
+  for (int i = y; (i < y+h); i++) {
     for (int j = x; (j < x+w); j++) {
       p_fb[wid*i+j] = pixels[w*(i-y)+(j-x)]; 
       
