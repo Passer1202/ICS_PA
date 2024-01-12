a8fbde7ff06fc7aeb5b2d3345efd56bcacaef1e5
tracer-ics2023 <tracer@njuics.org>
2024-01-12 12:14:15 +0800

    >  compile NEMU 221240073 李恒济 Linux hengji-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:14:15 up  3:59,  1 user,  load average: 0.66, 0.47, 0.44

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -33,13 +33,14 @@ int atoi(const char* nptr) {
 }
 
 void *malloc(size_t size) {
+
+assert(0);
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
   //panic("Not implemented");
   hbrk=(void*)ROUNDUP(heap.start,8) + heap_size;
-  assert(0);
   size  = (size_t)ROUNDUP(size, 8);//不确定8还是4
   char *addr = hbrk;
   hbrk += size;
