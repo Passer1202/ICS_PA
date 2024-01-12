0121c92fdb1f6deb37439c52914b45fa8293e1ab
tracer-ics2023 <tracer@njuics.org>
2024-01-12 10:03:28 +0800

    >  compile NEMU 221240073 李恒济 Linux hengji-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:03:28 up  1:48,  1 user,  load average: 0.57, 0.52, 0.58

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -7,7 +7,7 @@ void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
  // kbd->keydown = 0;
  // kbd->keycode = AM_KEY_NONE;
   
-  int32_t kc = inl(KBD_ADDR);
-  kbd->keydown = (kc & KEYDOWN_MASK) ? 1 : 0;
-  kbd->keycode = (kc & (~KEYDOWN_MASK));
+  int32_t keycode = inl(KBD_ADDR);
+  kbd->keydown = (keycode & KEYDOWN_MASK) ? 1 : 0;
+  kbd->keycode = (keycode & (~KEYDOWN_MASK));
 }
