c0c376200c3f4a32a7e7796a5d4247a9f7b8c48e
tracer-ics2023 <tracer@njuics.org>
2024-01-12 13:08:44 +0800

    >  compile NEMU 221240073 李恒济 Linux hengji-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:08:44 up  4:53,  1 user,  load average: 0.16, 0.28, 0.32

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -6,34 +6,29 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
- /*
-  int i;
-  int w = io_read(AM_GPU_CONFIG).width ; 
-  int h = io_read(AM_GPU_CONFIG).height;  
-  
-  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  for (i = 0; i < w * h; i ++) fb[i] = i;
-  outl(SYNC_ADDR, 1);
-  */
 }
 
-//static const int vga_mask=0xffff;
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-  
-  //uint32_t information=inl(VGACTL_ADDR);
-  uint16_t w=400;
-  uint16_t h=300;
+  // uint16_t w = (inl(VGACTL_ADDR) & 0xff00) >> 16;
+  // uint16_t h = inl(VGACTL_ADDR) & 0x00ff;
+
+  uint16_t w = 400;
+  uint16_t h = 300;
+
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = w, .height = h,
+    .width = w,
+    .height = h,
     .vmemsz = w * h * sizeof(uint32_t)
   };
 }
 
-void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+#include <klib.h>
 
+void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
- 
+  // uint16_t W = (inl(VGACTL_ADDR) & 0xff00) >> 16;
+  // uint16_t H = inl(VGACTL_ADDR) & 0x00ff;
   uint16_t W = 400;
   uint16_t H = 300;
 
@@ -41,10 +36,8 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   int y = ctl->y;
   int w = ctl->w;
   int h = ctl->h;
-  
   uint32_t * base = (uint32_t *) ctl->pixels;
   int cp_bytes = sizeof(uint32_t) * (w < W - x ? w : W - x);
-  
   for (int j = 0; j < h && y + j < H; ++j) {
       memcpy(&fb[(y + j) * W + x], base, cp_bytes);
       base += w;
@@ -55,9 +48,11 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
-  
+  // putstr("__am_gpu_fbdraw\n");
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
-  status->ready = true;
+    // status->ready = true;
+    status->ready = (bool) inl(SYNC_ADDR);
+    putstr("__am_gpu_status\n");
 }
