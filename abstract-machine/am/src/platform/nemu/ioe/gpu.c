a612db0776e734548927e1ba641fd416d6d0b224
tracer-ics2023 <tracer@njuics.org>
2024-01-12 13:34:39 +0800

    >  compile NEMU 221240073 李恒济 Linux hengji-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:34:39 up  5:19,  1 user,  load average: 0.20, 0.40, 0.36

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -32,19 +32,7 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
 
-  uint32_t wid=400;
-  int32_t w = ctl->w, h = ctl->h;
-  if (!ctl->sync && (w == 0 || h == 0))
-    return;
-  int32_t x = ctl->x, y = ctl->y;
-  uint32_t *pixels = ctl->pixels;
-  uint32_t *p_fb = (uint32_t *)(uintptr_t)FB_ADDR;//删掉了(uintptr_t)后ok
   
-  for (int i = y; (i < y+h); i++) {
-    for (int j = x; (j < x+w); j++) {
-      p_fb[wid*i+j] = pixels[w*(i-y)+(j-x)]; 
-    }
-  }
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);    
   }
