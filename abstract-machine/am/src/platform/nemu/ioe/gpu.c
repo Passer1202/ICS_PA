59b259ff9cd5ad30fd38732913e8ca113df79144
tracer-ics2023 <tracer@njuics.org>
2024-01-12 13:05:10 +0800

    >  compile NEMU 221240073 李恒济 Linux hengji-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:05:10 up  4:50,  1 user,  load average: 0.26, 0.38, 0.35

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -21,20 +21,19 @@ void __am_gpu_init() {
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   
   //uint32_t information=inl(VGACTL_ADDR);
-  uint32_t h=300;
-  uint32_t w=400;
+  uint16_t w=400;
+  uint16_t h=300;
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
     .width = w, .height = h,
-    .vmemsz = 0
+    .vmemsz = w * h * sizeof(uint32_t)
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
 
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  // uint16_t W = (inl(VGACTL_ADDR) & 0xff00) >> 16;
-  // uint16_t H = inl(VGACTL_ADDR) & 0x00ff;
+ 
   uint16_t W = 400;
   uint16_t H = 300;
 
@@ -42,8 +41,10 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   int y = ctl->y;
   int w = ctl->w;
   int h = ctl->h;
+  
   uint32_t * base = (uint32_t *) ctl->pixels;
   int cp_bytes = sizeof(uint32_t) * (w < W - x ? w : W - x);
+  
   for (int j = 0; j < h && y + j < H; ++j) {
       memcpy(&fb[(y + j) * W + x], base, cp_bytes);
       base += w;
