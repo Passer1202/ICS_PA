eebe9e3d72deea0fa08827f344c7e16c6f0bf842
tracer-ics2023 <tracer@njuics.org>
2024-01-12 10:53:13 +0800

    >  compile NEMU 221240073 李恒济 Linux hengji-virtual-machine 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:53:13 up  2:38,  1 user,  load average: 0.17, 0.19, 0.18

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,12 +4,23 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
+  int i;
+  int w = io_read(AM_GPU_CONFIG).width / 32;  // TODO: get the correct width
+  int h = io_read(AM_GPU_CONFIG).height / 32;  // TODO: get the correct height
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i ++) fb[i] = i;
+  outl(SYNC_ADDR, 1);
+  
 }
 
+static const int vga_mask=0xffff;
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
+  
+  uint32_t information=inl(VGACTL_ADDR);
+  
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
+    .width = information&vga_mask, .height = information>>16,
     .vmemsz = 0
   };
 }
