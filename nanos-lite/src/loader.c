e473232bc623fd72436f6c8082f38b6f0c34a724
tracer-ics2023 <tracer@njuics.org>
2024-01-13 16:36:00 +0800

    >  compile NEMU 221240073 李恒济 Linux hengji-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:36:00 up 55 min,  1 user,  load average: 0.24, 0.16, 0.17

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,8 +9,40 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+
+//未进行ISA类型检查，后续或补
+
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+
+//Ehdr----header
+//Phdr----segement
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
+	
+	Elf_Ehdr ehdr;
+	ramdisk_read(&ehdr,0,sizeof(Elf_Ehdr));
+	//check the magic number
+	assert(*(uint32_t *)ehdr.e_ident == 0x464c457f);
+	
+	//读取段信息
+	Elf_Phdr phdr[ehdr.e_phnum];
+	ramdisk_read(phdr, ehdr.e_phoff, sizeof(Elf_Phdr)*ehdr.e_phnum);
+	
+	//处理段信息
+	for(int i=0;i<ehdr.e_phnum;i++){
+		if(phdr[i].p_type!=PT_LOAD)continue;//检测是否是需要加载的段
+		//加载对应段到主存
+		assert(phdr[i].p_type==PT_LOAD);
+		ramdisk_read((void*)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_memsz);
+		//将[VirtAddr + FileSiz, VirtAddr + MemSiz)对应的物理区间清零
+		memset((void*)(phdr[i].p_vaddr+phdr[i].p_filesz), 0, phdr[i].p_memsz - phdr[i].p_filesz);
+
+
+	}
+
+
+
+		
   return 0;
 }
 
