09e8dc733d174be2b266f63a8be4480264d69c8f
tracer-ics2023 <tracer@njuics.org>
2024-01-14 09:59:12 +0800

    >  compile NEMU 221240073 李恒济 Linux hengji-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:59:12 up 35 min,  1 user,  load average: 0.21, 0.20, 0.15

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -145,7 +145,7 @@ static int decode_exec(Decode *s) {
   /*------ECALL-----*/   //感觉和ebreak很像，先放到N型来识别
   
   
-  INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , N, bool success;s->dnpc = (isa_raise_intr(isa_reg_str2val("a7", &success), s->pc)); );//跳转到异常入口地址
+  INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , N, s->dnpc = (isa_raise_intr(11, s->pc)); );//跳转到异常入口地址
   //bool success;s->dnpc = (isa_raise_intr(isa_reg_str2val("a7", &success), s->pc));
   //INSTPAT("0011000 00010 00000 000 00000 11100 11", mret  ,  N, s->dnpc = cpu.csr.mepc );//从异常处理过程中返回
   /*-------N-------*/
