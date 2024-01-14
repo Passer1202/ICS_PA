4024d37d73f9d226f98d756be61b0ee64ce72e51
tracer-ics2023 <tracer@njuics.org>
2024-01-14 19:18:53 +0800

    >  compile NEMU 221240073 李恒济 Linux hengji-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:18:53 up  4:03,  1 user,  load average: 0.73, 0.44, 0.33

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -23,7 +23,6 @@ int fs_close(int fd);
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
-  assert(0);
   switch (a[0]) {
     case SYS_yield: c->GPRx=sys_yield(); break;
     case SYS_exit:  sys_exit(c->GPR2); break;
