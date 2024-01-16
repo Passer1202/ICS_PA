275c06cf7f14fb2ac2bfc04b4f349cea247ada74
tracer-ics2023 <tracer@njuics.org>
2024-01-16 12:28:04 +0800

    >  compile NEMU 221240073 李恒济 Linux hengji-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:28:04 up  1:55,  1 user,  load average: 0.63, 0.88, 0.59

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -130,8 +130,8 @@ void NDL_OpenCanvas(int *w, int *h) {
   }
   canvas_w=*w;
   canvas_h=*h;
-  canvas_x=(screen_w-canvas_w)/2+1;
-  canvas_y=(screen_h-canvas_h)/2;
+  canvas_x=(screen_w-canvas_w)/2+10;
+  canvas_y=(screen_h-canvas_h)/2+10;
   
   assert(canvas_w + canvas_x <= screen_w&& canvas_h + canvas_y <= screen_h);
 }
