388c7fe4670a23963250e217ad35e8b0e696e036
tracer-ics2023 <tracer@njuics.org>
2024-01-17 20:50:20 +0800

    >  compile NEMU 221240073 李恒济 Linux hengji-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:50:20 up  4:16,  1 user,  load average: 1.17, 0.48, 0.28

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -37,35 +37,48 @@ int SDL_PollEvent(SDL_Event *ev) {
   	//else assert(0);//确保没有其他事件
   	
   	//assert(0);
+  	int mark=0;
   	uint32_t i=0;
   	uint32_t bound=sizeof(keyname) / sizeof(keyname[0]);
   	
   	for(;i<bound;i++){
   		
-  		if(strncmp(event_buf + 3, keyname[i], strlen(event_buf) - 4) != 0) continue;//偏移量或待调试
-  		
+  		if(strncmp(event_buf + 3, keyname[i], strlen(event_buf) - 4) != 0||strlen(keyname[i])!=strlen(event_buf)-4) continue;//偏移量或待调试
+  		mark=1;
   		ev->key.keysym.sym = i;
   	}
+  	
+  	
   	
   	return 1;
   	
   }
-  else{
-  //未读取有事件
-  	ev->key.keysym.sym = 0;
-  	ev->key.type = SDL_USEREVENT; 
-  
-  }
-  //assert(0);
-  //free(event_buf);
-  //assert(0);
   
   return 0;
 }
 
 int SDL_WaitEvent(SDL_Event *event) {
-  SDL_PollEvent(event);
-  return 1;
+	
+	char wait_buf[32];
+	while(NDL_PollEvent(wait_buf,32)==0);
+	
+	if(strncmp(wait_buf,"ku",2)==0)
+		event->key.type=SDL_KEYUP;
+	else
+		event->key.type=SDL_KEYDOWN;
+	
+	int mark=0;
+	
+	uint32_t i=0;
+  	uint32_t bound=sizeof(keyname) / sizeof(keyname[0]);
+  	
+  	for(;i<bound;i++){
+  		
+  		if(strncmp(wait_buf + 3, keyname[i], strlen(wait_buf) - 4) != 0||strlen(keyname[i])!=strlen(wait_buf)-4) continue;//偏移量或待调试
+  		mark=1;
+  		event->key.keysym.sym = i;
+  	}
+	return 1;
 }
 
 int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
