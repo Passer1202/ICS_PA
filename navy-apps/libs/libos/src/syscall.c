33ce638e4320d392a33da385babc245ca5e3291c
tracer-ics2023 <tracer@njuics.org>
2024-01-17 17:47:42 +0800

    >  compile NEMU 221240073 李恒济 Linux hengji-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:47:42 up  1:14,  1 user,  load average: 0.60, 0.50, 0.49

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -81,9 +81,9 @@ int _write(int fd, void *buf, size_t count) {
 
 void *_sbrk(intptr_t increment) {
 
-  static char* program_break=&_end;
-  char* addr=program_break+increment;
-  assert(0);
+  static int program_break=&_end;
+  int addr=program_break+increment;
+  
   if(_syscall_(SYS_brk, addr, 0, 0)==0){
   	char* ret=program_break;
   	program_break=addr;
