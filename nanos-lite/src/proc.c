7ea4bf55f664ac66a558ae7b56022fec838a3a53
tracer-ics2023 <tracer@njuics.org>
2024-01-17 18:32:48 +0800

    >  compile NEMU 221240073 李恒济 Linux hengji-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:32:48 up  1:59,  1 user,  load average: 0.58, 0.26, 0.23

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -30,7 +30,7 @@ void init_proc() {
 
   // load program here
   //printf("%d",get_ramdisk_size());
-  const char filename[] = "/bin/nslider";
+  const char filename[] = "/bin/event-test";
   naive_uload(NULL, filename);
 }
 
