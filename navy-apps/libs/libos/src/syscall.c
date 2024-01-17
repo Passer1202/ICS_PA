675f442bf7396ca8d69d8450b64de7a4023797fa
tracer-ics2023 <tracer@njuics.org>
2024-01-17 17:48:21 +0800

    >  compile NEMU 221240073 李恒济 Linux hengji-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:48:21 up  1:14,  1 user,  load average: 0.64, 0.52, 0.49

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -46,7 +46,7 @@
 #endif
 
 
-extern char _end;//_end标记
+extern char end;//_end标记
 
 intptr_t _syscall_(intptr_t type, intptr_t a0, intptr_t a1, intptr_t a2) {
   register intptr_t _gpr1 asm (GPR1) = type;
@@ -81,8 +81,8 @@ int _write(int fd, void *buf, size_t count) {
 
 void *_sbrk(intptr_t increment) {
 
-  static int program_break=&_end;
-  int addr=program_break+increment;
+  static char* program_break=&end;
+  char* addr=program_break+increment;
   
   if(_syscall_(SYS_brk, addr, 0, 0)==0){
   	char* ret=program_break;
