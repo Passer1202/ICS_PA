1830c9b29f6d0dc1fa7e151b4357fea869a17cea
tracer-ics2023 <tracer@njuics.org>
2024-01-18 15:24:48 +0800

    >  compile NEMU 221240073 李恒济 Linux hengji-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:24:48 up 37 min,  1 user,  load average: 0.31, 0.28, 0.26

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -7,6 +7,7 @@
 void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_Rect *dstrect) {
   	assert(dst && src);
 	assert(dst->format->BitsPerPixel == src->format->BitsPerPixel);
+  	if(dst->format->palette!=NULL)assert(0);
   	
   	uint32_t * src_p=(uint32_t*)src->pixels;
   	uint32_t * dst_p=(uint32_t*)dst->pixels;
@@ -67,6 +68,8 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
 }
 
 void SDL_FillRect(SDL_Surface *dst, SDL_Rect *dstrect, uint32_t color) {
+	
+	if(dst->format->palette!=NULL)assert(0);
 	uint32_t *buf=(uint32_t*)dst->pixels;
 	if(dstrect==NULL){
 		int i=0;
