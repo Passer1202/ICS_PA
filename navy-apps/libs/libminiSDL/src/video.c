d84f897e5f5f4d285ad271b8d0389209a7487d19
tracer-ics2023 <tracer@njuics.org>
2024-01-18 16:25:55 +0800

    >  compile NEMU 221240073 李恒济 Linux hengji-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:25:55 up  1:38,  1 user,  load average: 0.34, 0.41, 0.37

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -70,8 +70,8 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
   	}
   	else{
   		
-  		uint8_t * src_p=(uint8_t*)src->pixels;
-	  	uint8_t * dst_p=(uint8_t*)dst->pixels;
+  		uint32_t * src_p=(uint32_t*)src->pixels;
+	  	uint32_t * dst_p=(uint32_t*)dst->pixels;
 	  	
 	  	int s_w=src->w;
 	  	int s_h=src->h;
@@ -97,7 +97,7 @@ void SDL_BlitSurface(SDL_Surface *src, SDL_Rect *srcrect, SDL_Surface *dst, SDL_
 	  				int index_src_p=i*s_w+j;
 	  				dst_p[index_dst_p]=src_p[index_src_p];
 	  			}
-	  			//assert(0);
+	  			assert(0);
 	  		return ;
 	  	}
 	  	
