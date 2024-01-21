44b7334a0087f57a9b40b8505cec154c740a7b46
tracer-ics2023 <tracer@njuics.org>
2024-01-21 10:11:46 +0800

    >  compile NEMU 221240073 李恒济 Linux hengji-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:11:46 up 29 min,  1 user,  load average: 0.50, 0.28, 0.22

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -28,7 +28,7 @@ void hello_fun(void *arg) {
 void init_proc() {
   
   context_kload(&pcb[0], hello_fun, "Begin");
-  char *argv[] = {"1",NULL};
+  char *argv[] = {"/bin/menu",NULL};
   char *envp[] = {NULL};
   context_uload(&pcb[1], "/bin/menu", argv, envp);
   
