a3830dc9e52c8c742f2edb32392547d7c8d9eba6
tracer-ics2023 <tracer@njuics.org>
2024-01-21 10:06:40 +0800

    >  compile NEMU 221240073 李恒济 Linux hengji-virtual-machine 6.5.0-14-generic #14~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Nov 20 18:15:30 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:06:40 up 23 min,  1 user,  load average: 0.45, 0.29, 0.22

--- navy-apps/tests/exec-test/main.c
+++ navy-apps/tests/exec-test/main.c
@@ -4,13 +4,13 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-assert(0);
+
   int n = (argc >= 2 ? atoi(argv[1]) : 1);
   
   printf("%s: argv[1] = %d\n", argv[0], n);
   char buf[16];
   sprintf(buf, "%d", n + 1);
-  
+  while(1);
   execl(argv[0], argv[0], buf, NULL);
   return 0;
 }
